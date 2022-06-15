46c6356188de6c277c1056b933cf91b233f4860f
facundo <facundocufre02@gmail.com>
2022-06-14 22:41:52 -0300

    correcciones desafio 9 context

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,13 +1,15 @@
 import './cartlist.css'
 import CartContext from '../Context/CartContext'
-import { useContext } from 'react'
-import { ListAlt } from '@mui/icons-material'
+import { useContext, useState } from 'react'
+
+
 
 const CartLIst = ()=> {
+    const [refresh, setRefresh] = useState(1)
     const { cartListItems, quitProductFromCart, clearCart } = useContext(CartContext)
     return(
         <section className='carrito'>
-            <div className='vaciar' onClick={() => clearCart()}>VACIAR CARRITO</div>
+            <div className='vaciar' onClick={() => {clearCart(); setRefresh(refresh + 1)}}>VACIAR CARRITO</div>
             {cartListItems.length === 0 && 
                 <span>NO HAY PRODUCTOS EN EL CARRITO</span>
             }
@@ -22,7 +24,7 @@ const CartLIst = ()=> {
                                     <span>{item.autor}</span>
                                     <span>{item.precio}</span>
                                     <span>{item.countQuantity}</span>
-                                    <span onClick={() => quitProductFromCart(item.id)}>ELIMINAR</span>
+                                    <span onClick={() => {quitProductFromCart(item.id); setRefresh(refresh + 1)}}>ELIMINAR</span>
                             </div>
                         </div> 
                     )
